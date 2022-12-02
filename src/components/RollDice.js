a7c081ea93a41b2e17d3e39840e66e8248e071c5
Cheung <cheung@tdfcu.com>
2022-12-02 16:11:15 -0500

    implmented button to change state of die

--- src/components/RollDice.js
+++ src/components/RollDice.js
@@ -1,12 +1,19 @@
 import Die from "./Die";
+import { useState } from "react";
 const RollDice = () => {
+  const [roll, setRoll] = useState("one");
   const numbers = ["one", "two", "three", "four", "five", "six"];
   const random = Math.floor(Math.random() * 6);
   const randomNumber = numbers[random];
 
+  const handleClick = () => {
+    setRoll(randomNumber);
+  };
+
   return (
     <div>
-      <Die number={randomNumber} />
+      <Die number={roll} />
+      <button onClick={handleClick}>Roll Dice</button>
     </div>
   );
 };
